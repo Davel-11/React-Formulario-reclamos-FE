47f2dffc6a612a45d2cce647fc1e39b440c4768a
Davel <davelaltan@gmail.com>
2021-01-05 15:36:14 -0600

    Before going to sectiong 5 // on this commit we have created Lists, delete, update with two way bindingcopy arrays and objects, etc

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ class App extends Component {
 
   state =  {
     persons: [
-      { name: 'Chris', age: 20 },
-      { name: 'Saul', age: 210 },
-      { name: 'Ronaldo', age: 30 }
+      { id: 1,  name: 'Chris', age: 20 },
+      { id: 2, name: 'Saul', age: 210 },
+      { id: 3, name: 'Ronaldo', age: 30 }
     ],
-    otherState: 'some value'
+    otherState: 'some value',
+    showPersons: false
   };
 
   switchNames = (newName) => {
@@ -27,39 +28,85 @@ class App extends Component {
 
   }
 
-  nameChangeHandler = (event) =>  {
+  nameChangeHandler = (event, id) =>  {
 
-    this.setState({
-      persons: [
-        { name: 'ChrisXX', age: 20 },
-        { name: event.target.value, age: 210 },
-        { name: 'RonaldoX', age: 30 }
-      ]
-    });
+    // 1 -- find person 
+    const foundPersonIndex = this.state.persons.findIndex( obj => obj.id === id );
 
+    // 2 -- copy person properties
+    const newPerson = {...this.state.persons[foundPersonIndex]};
+
+    // update valie live
+    newPerson.name = event.target.value;
+
+    // update array
+    const persons = [...this.state.persons];
+    persons[foundPersonIndex] = newPerson;
+
+    // update State
+    this.setState({ persons: persons })
+
+  }
+
+  togglePersonsHandler = () => {
+
+    const showIt = this.state.showPersons;
+    
+    this.setState({ showPersons: !showIt });
+  }
+
+  delPerson = ( id ) => {
+
+      const tempArray =  [...this.state.persons];
+      tempArray.splice(id, 1);
+      this.setState( { persons: tempArray } )
   }
   
   render() { 
+
+    const style = {
+        backgroundColor: 'white',
+        border: '1px solid blue',
+        cursor: 'pointer'
+    }
+
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+          <div >
+            {
+              this.state.persons.map( (person, index) =>  {
+                console.log('person is', person);
+                return <Person
+                    click={this.delPerson.bind(this, index)}
+                    name={person.name}
+                    age={person.age}
+                    key={person.id} 
+                    changed={ (event) => this.nameChangeHandler(event, person.id ) } />
+              })
+            }
+          </div>
+      );
+    }
+
     return(
 
       <div className="App">
-        
+
           <h1>Hi, Mi firsty app</h1>
   
-          <button onClick={ () => this.switchNames('Performance Chris New Name ') } >click me! </button>
-  
-          <Person name={this.state.persons[0].name} age="30" ></Person>
-  
-          <Person 
-            name={this.state.persons[1].name} 
-            age="25"
-            click={this.switchNames.bind(this, 'use Binding')}
-            changed={this.nameChangeHandler} >
-            My Hobies: Racing 
-          </Person>
-  
-          <Person name={this.state.persons[2].name} age="16"></Person>
+          {/* onClick={ () => this.switchNames('Performance Chris New Name ') } >  */}
 
+          <button 
+            style ={style}
+            onClick={this.togglePersonsHandler} >  
+            Show/Hide Persons 
+          </button>
+
+          {persons}
+          
+  
       </div>
     )
   }
@@ -114,3 +161,44 @@ export default App;
   }
 
 */
+
+
+/* *********** ANOTHER IF TYPE
+
+  {
+    this.state.showPersons === true ?
+
+      <div >
+
+          <Person name={this.state.persons[0].name} age="30" ></Person>
+
+          <Person 
+            name={this.state.persons[1].name} 
+            age="25"
+            click={this.switchNames.bind(this, 'use Binding')}
+            changed={this.nameChangeHandler} >
+            My Hobies: Racing 
+          </Person>
+
+          <Person name={this.state.persons[2].name} age="16"></Person>
+
+      </div>
+    :
+      null
+  }
+
+*/
+
+
+/*  ******* OLD PERSONS ******
+  <Person name={this.state.persons[0].name} age="30" ></Person>
+  <Person 
+    name={this.state.persons[1].name} 
+    age="25"
+    click={this.switchNames.bind(this, 'use Binding')}
+    changed={this.nameChangeHandler} >
+    My Hobies: Racing 
+  </Person>
+  <Person name={this.state.persons[2].name} age="16"></Person>
+*/
+
